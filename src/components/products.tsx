8fc90c28ef23c5415cc84ebc7494687223a4602c
samhoooo <samho1996@gmail.com>
2022-05-17 22:51:06 +0800

    add alt on image

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -16,7 +16,7 @@ export default function Products(props: IProductProps) {
                     props.products.map((item) => {
                         return (
                             <div className="item" data-testid={`product-${item.id}`} key={item.id}>
-                                <img src={item.image} className="product-image"/>
+                                <img src={item.image} alt={item.name} className="product-image"/>
                                 <div data-testid={`product-name-${item.id}`}>{item.name}</div>
                                 <div data-testid={`product-price-${item.id}`}>{formatCurrency(item.price)}</div>
                                 <button className="button add-to-cart" data-testid={`add-button-${item.id}`} onClick={() => { props.addProductToCart(item)}}>Add to Cart</button>
